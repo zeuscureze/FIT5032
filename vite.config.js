2446783e612d965603d6e1335e44d947100afdd9
zeuscureze <juliangg@student.unimelb.edu.au>
2024-10-18 22:42:43 +1100

    12

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     vueDevTools(),
@@ -14,5 +13,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  base: mode === 'production'
+    ? '/<repository-name>/'  // 替换为你GitHub仓库的名称
+    : '/'
+}))
+
